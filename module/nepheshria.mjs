7519bba18b87b36062b1afcec151ea998e5810ab
Bloodchild-Game <justinvillerot@orange.fr>
2023-01-05 22:16:19 +0100

    Weapon item Sheet #3

--- module/nepheshria.mjs
+++ module/nepheshria.mjs
@@ -1,8 +1,9 @@
 import NepheshriaItemSheet from "./sheets/nepheshriaItemSheets.mjs";
+import {nepheshria} from "./config.mjs";
 
 Hooks.once("init", async function () {
     console.log("nepheshria | Initialising Nepheshria System");
-
+    CONFIG.nepheshria = nepheshria;
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("nepheshria", NepheshriaItemSheet, { makeDefault: true});
 });
--- module/sheets/nepheshriaItemSheets.mjs
+++ module/sheets/nepheshriaItemSheets.mjs
@@ -3,4 +3,12 @@ export default class NepheshriaItemSheet extends ItemSheet {
         const path = "systems/nepheshria/templates/item";
         return `${path}/${this.item.type}-sheet.html`;
     }
+
+    getData() {
+        const data = super.getData();
+
+        data.config = CONFIG.nepheshria;
+
+        return data;
+    }
 }
