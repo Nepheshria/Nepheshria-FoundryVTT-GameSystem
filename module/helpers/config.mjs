189755959ab9bee9ecffb6b7ddc5021573a7d322
Villerot Justin <justinvillerot@orange.fr>
2023-01-06 22:00:16 +0100

    Weapon Item Sheet #3

---
+++ module/helpers/config.mjs
@@ -0,0 +1,44 @@
+export const NEPHESHRIA = {};
+
+/**
+ * The set of Damage Types used within the system.
+ * @type {Object}
+ */
+NEPHESHRIA.attackTypes = {
+    acid: "NEPHESHRIA.attackType.acid",
+    bludgeoning: "nepheshria.attackType.bludgeoning",
+    cold: "NEPHESHRIA.attackType.cold",
+    fire: "NEPHESHRIA.attackType.fire",
+    force: "NEPHESHRIA.attackType.force",
+    lightning: "NEPHESHRIA.attackType.lightning",
+    necrotic: "NEPHESHRIA.attackType.necrotic",
+    piercing: "NEPHESHRIA.attackType.piercing",
+    poison: "NEPHESHRIA.attackType.poison",
+    psychic: "NEPHESHRIA.attackType.psychic",
+    slashing: "NEPHESHRIA.attackType.slashing",
+    soul: "NEPHESHRIA.attackType.soul"
+}
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+NEPHESHRIA.ability = {
+    str: "NEPHESHRIA.ability.str",
+    dex: "NEPHESHRIA.ability.dex",
+    con: "NEPHESHRIA.ability.con",
+    soc: "NEPHESHRIA.ability.soc",
+    inst: "NEPHESHRIA.ability.inst",
+    mag: "NEPHESHRIA.ability.mag",
+    int: "NEPHESHRIA.ability.int",
+}
+
+NEPHESHRIA.abilityAbbreviations = {
+    str: "NEPHESHRIA.abilityAbbr.str",
+    dex: "NEPHESHRIA.abilityAbbr.dex",
+    con: "NEPHESHRIA.abilityAbbr.con",
+    soc: "NEPHESHRIA.abilityAbbr.soc",
+    inst: "NEPHESHRIA.abilityAbbr.inst",
+    mag: "NEPHESHRIA.abilityAbbr.mag",
+    int: "NEPHESHRIA.abilityAbbr.int",
+};
--- module/sheets/nepheshriaItemSheets.mjs
+++ module/sheets/nepheshriaItemSheets.mjs
@@ -1,14 +1,56 @@
-export default class NepheshriaItemSheet extends ItemSheet {
+/**
+ * Extend the basic ItemSheet with some very simple modifications
+ * @extends {ItemSheet}
+ */
+export class NepheshriaItemSheet extends ItemSheet {
+
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["nepheshria", "sheet", "item"],
+            width: 520,
+            height: 480,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+        });
+    }
+
+    /** @Override */
     get template() {
+        // template link
         const path = "systems/nepheshria/templates/item";
+        // return template corresponding to item type
         return `${path}/${this.item.type}-sheet.html`;
     }
 
+    /* -------------------------------------------- */
+
     getData() {
         const data = super.getData();
+        const dataClone = data.item;
 
-        data.config = CONFIG.nepheshria;
+        // Retrieve the roll data for TinyMCE editors.
+        data.rollData = {};
+        let actor = this.object?.parent ?? null;
+        if (actor) {
+            data.rollData = actor.getRollData();
+        }
+
+        // Add the actor's data to context.data for easier access, as well as flags.
+        data.system = dataClone.system;
+        data.flags = dataClone.flags;
 
         return data;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Everything below here is only needed if the sheet is editable
+        if (!this.isEditable) return;
+
+        // Roll handlers, click handlers, etc. would go here.
     }
 }
